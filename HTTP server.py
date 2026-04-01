ea8c860aba81bf2e457e3d4eee7c5f11344171c4
pastrie <152612069+pastry16@users.noreply.github.com>
2026-04-01 21:18:51 +0530

    Update HTTP server.py

--- HTTP server.py
+++ HTTP server.py
@@ -5,5 +5,27 @@
 import http.server
 import socketserver
 
+# socketserver and http.server are built-in modules
+
 PORT = 8000
+
+# by default, servers start on port 8000, taking files from the directory
+
 Handler = http.server.simplehttphandler
+
+# simplehttphandler is the default handler that serves files and directories
+
+with socketserver.TCPserver (("", PORT), Handler) as httpd
+
+# socketserver is used to build a http server in python other than the one-liner method
+# the above cmd creates a TCP server. the argument helps in accessing all network interfaces available in that port
+# this makes the server accessible to all devices under the local network
+
+print (f"Serving at port {PORT}") 
+
+# this command starts the server at the directed port we put earlier.
+
+httpd.serve_forever()
+
+# this command tells the server to be active until we manually stop it.
+# and that's all, bye
