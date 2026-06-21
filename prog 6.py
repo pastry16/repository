28ccc4b35da905b4fb2724bc549f718bbecd01e4
pastrie <152612069+pastry16@users.noreply.github.com>
2026-06-21 15:33:06 +0530

    Update prog 6.py

--- prog 6.py
+++ prog 6.py
@@ -1,6 +1,7 @@
 def addElements(list1):
 	newList = list1
 	inp = input ("Do you want to add any new element to the list? (Y/N)")
+	
 	if (inp == 'Y' or inp =='y'):
 		elem = int(input("Enter the elements:"))
 		index = int(input("Enter the index at which you would like to add the elements:"))
@@ -8,11 +9,14 @@ def addElements(list1):
 		print("***Element added***")
 		addElements(newList)
 		return newList
+
 list1 = []
 inp = int(input("How many elements do you want to add in the list?"))
+
 for i in range(inp):
 	a = int(input("Enter the elements:"))
 	list1.append(a)
 	print ("The list entered is:", list1)
+
 modList = addElements(list1)
 print ("The modified list is:", modList)
