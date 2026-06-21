f62fb4501ce0588c38118d6dc3a00a5b8cc0bf2b
pastrie <152612069+pastry16@users.noreply.github.com>
2026-06-21 12:39:45 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,38 @@
+#program to find square of a number
+num = int(input("enter a number"))
+square = num * num
+print (square)
+
+#program to find cube of a number
+num1 = int(input("enter a number"))
+cube = num1 * num1 * num1
+print (cube)
+
+#program to find circumference of a circle
+r = int(input("enter a radius"))
+circumference = 2 * 3.14 * r
+print (circumference)
+
+#program to print message n times
+message = str(input("enter a message"))
+n = int(input("enter a number"))
+print (message * n)
+
+#program to find area of a rectangle
+length = int(input("enter a length"))
+breadth= int(input("enter a breadth"))
+area = length * breadth
+print (area)
+
+#program to find area of triangle
+base = int(input("enter a base"))
+height = int(input("enter a height"))
+area2 = 1/2 * base * height
+print (area2)
+
+#program to find sum of 3 numbers
+num2 = int(input("enter a number"))
+num3 = int(input("enter a number"))
+num4 = int(input("enter a number"))
+sum = num2 + num3 + num4
+print (sum)
---
+++ program6prac.py
@@ -0,0 +1,18 @@
+def addElements(list1):
+	newList = list1
+	inp = input ("Do you want to add any new element to the list? (Y/N)")
+	if (inp == 'Y' or inp =='y'):
+		elem = int(input("Enter the elements:"))
+		index = int(input("Enter the index at which you would like to add the elements:"))
+		newList.insert(index,elem)
+		print("***Element added***")
+		addElements(newList)
+		return newList
+list1 = []
+inp = int(input("How many elements do you want to add in the list?"))
+for i in range(inp):
+	a = int(input("Enter the elements:"))
+	list1.append(a)
+	print ("The list entered is:", list1)
+modList = addElements(list1)
+print ("The modified list is:", modList)
