0e16348ed9c3523f24aa297996b6531e0e269bb3
pastrie <152612069+pastry16@users.noreply.github.com>
2026-08-01 16:09:44 +0530

    Create draft.py

---
+++ draft.py
@@ -0,0 +1,16 @@
+#program to print a list containing elements less than 10
+
+l=list()
+
+i= int(input("How many elements do you want to add in the list?"))
+
+for i in range (a):
+  a = int(input("Enter the elements"))
+  
+  if (a < 10):
+    l.append(a)
+
+  else:
+    print("This element is out of range")
+
+print("The final list is", l)
